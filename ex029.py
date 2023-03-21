d3aa51df98506903857a9e78ae97d8e86e898a93
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-03-21 16:14:47 -0300

    Add files via upload

---
+++ ex029.py
@@ -0,0 +1,8 @@
+velocidade = float(input('Qual é a velocidade atual do carro? '))
+if velocidade > 80:
+    print('MULTADO! Você excedeu o limite permitido que é de 80km/h')
+    multa = (velocidade - 80) * 7
+    print('Você deve pagar uma multa de R${:.2f}!'.format(multa))
+print('Tenha um bom dia! Dirija com segurança!')
+
+#sendo R$7 a mais a cada km acima do limite
